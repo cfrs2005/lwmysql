3cb8f936066b0d66ccc01c9f3ff00c1425840668
qyzhang <cfrs2005@yahoo.com>
2014-11-27 18:34:08 +0800

    feature:
    修复执行shell脚本的时候 出错退出

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@
 if [ ! -f /root/a.log ]; then
   /usr/bin/mysqld_safe &
   sleep 10s
-  MYSQL_PASSWORD=`lewa`
-  mysqladmin -u root password $MYSQL_PASSWORD
-  mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-  mysql -uroot -p$MYSQL_PASSWORD < /root/mysql.sql
+  MYSQL_PASSWORD = `lewa`
+  mysqladmin -u root password lewa
+  mysql -uroot -plewa -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'lewa' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+  mysql -uroot -plewa < /root/mysql.sql
   killall mysqld
-  echo "done" > /root/a.log  
+  echo "done" > /root/a.log
 fi
 
 
